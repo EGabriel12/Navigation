e78568366fd79ea7339d4667301ae535232398f2
Elias Gabriel <eliasgab@motorola.com>
2024-11-07 22:38:50 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Elias Gabriel dos Santos Correa on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
